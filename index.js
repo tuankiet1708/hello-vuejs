d84406bdd82c7281d23abd40d22817bded00c889
Kiet Tran <tuankiet1708@gmail.com>
2019-10-17 19:53:57 +0700

    Handling User Input

--- index.js
+++ index.js
@@ -31,3 +31,23 @@ var app4 = new Vue({
       ]
     }
 })
+
+// Handling User Input
+var app5 = new Vue({
+    el: '#app-5',
+    data: {
+      message: 'Hello Vue.js!'
+    },
+    methods: {
+      reverseMessage: function () {
+        this.message = this.message.split('').reverse().join('')
+      }
+    }
+})
+
+var app6 = new Vue({
+    el: '#app-6',
+    data: {
+      message: 'Hello Vue!'
+    }
+})
