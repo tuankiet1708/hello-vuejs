eed0161e1d01ab7e526e2a521437274c7520e459
Kiet Tran <tuankiet1708@gmail.com>
2019-10-21 22:09:58 +0700

    Content Distribution with Slots

--- index.js
+++ index.js
@@ -269,3 +269,51 @@ new Vue({
     }
   }
 })
+
+// Component Basics
+Vue.component('blog-post', {
+  props: ['post'],
+  template: `
+    <div class="blog-post">
+      <h3>{{ post.title }}</h3>
+      <button v-on:click="$emit('enlarge-text', 0.1)">
+        Enlarge text
+      </button>
+      <div v-html="post.content"></div>
+    </div>
+  `
+})
+Vue.component('custom-input', {
+  props: ['value'],
+  template: `
+    <input
+      v-bind:value="value"
+      v-on:input="$emit('input', $event.target.value)"
+    >
+  `
+})
+new Vue({
+  el: '#blog-post',
+  data: {
+    posts: [
+      { id: 1, title: 'My journey with Vue' },
+      { id: 2, title: 'Blogging with Vue' },
+      { id: 3, title: 'Why Vue is so fun' }
+    ],
+    postFontSize: 1,
+    searchText: "Hello"
+  }
+})
+
+
+// Content Distribution with Slots
+Vue.component('alert-box', {
+  template: `
+    <div class="demo-alert-box">
+      <strong>Error!</strong>
+      <slot></slot>
+    </div>
+  `
+})
+new Vue({el: "#slots-demo"})
+
